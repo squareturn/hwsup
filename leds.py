cf4a75068672fd2bd6c491ea965e3290d2f00c38
Steven Pelletier <steven.pelletier@squareturn.tech>
2017-06-05 18:39:03 +0000

    delete code not related to the POE HAT. add support for indicator LED on RJ45 connector

--- leds.py
+++ leds.py
@@ -7,7 +7,6 @@ LED_RED = 7			# 3 color led on POE HAT
 LED_GREEN = 22			# 3 color led on POE HAT
 LED_BLUE = 9			# 3 color led on POE HAT
 LED_RJ45 = 25			# second green led on rj45 connector
-LED_PI_RED = 35			# on some raspberry pi model PCBs
 
 def	initialize():
 	GPIO.setwarnings(False)
@@ -16,15 +15,12 @@ def	initialize():
 	GPIO.setup(LED_GREEN, GPIO.OUT)
 	GPIO.setup(LED_BLUE, GPIO.OUT)
 	GPIO.setup(LED_RJ45, GPIO.OUT)
-	GPIO.setup(LED_PI_RED, GPIO.OUT)
 
 def	led(value):
+	GPIO.output(LED_RJ45, value & 8 != 0)
 	GPIO.output(LED_RED, value & 4 != 0)
 	GPIO.output(LED_GREEN, value & 2 != 0)
 	GPIO.output(LED_BLUE, value & 1 != 0)
-
-# GPIO.output(LED_RJ45, 0)
-# GPIO.output(LED_PI_RED, 0)
 
 def main():
 	initialize()
