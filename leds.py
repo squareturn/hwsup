1b392369161172345e78be4e8848ab909df32184
Steven Pelletier <steven.pelletier@squareturn.tech>
2017-06-05 18:31:13 +0000

    renamed to match usage.

---
+++ leds.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import	RPi.GPIO as GPIO
+import	sys
+
+LED_RED = 7			# 3 color led on POE HAT
+LED_GREEN = 22			# 3 color led on POE HAT
+LED_BLUE = 9			# 3 color led on POE HAT
+LED_RJ45 = 25			# second green led on rj45 connector
+LED_PI_RED = 35			# on some raspberry pi model PCBs
+
+def	initialize():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED_RED, GPIO.OUT)
+	GPIO.setup(LED_GREEN, GPIO.OUT)
+	GPIO.setup(LED_BLUE, GPIO.OUT)
+	GPIO.setup(LED_RJ45, GPIO.OUT)
+	GPIO.setup(LED_PI_RED, GPIO.OUT)
+
+def	led(value):
+	GPIO.output(LED_RED, value & 4 != 0)
+	GPIO.output(LED_GREEN, value & 2 != 0)
+	GPIO.output(LED_BLUE, value & 1 != 0)
+
+# GPIO.output(LED_RJ45, 0)
+# GPIO.output(LED_PI_RED, 0)
+
+def main():
+	initialize()
+	if len(sys.argv) == 1:
+		led(0)
+	else:
+		led(int(sys.argv[1]))
+
+if __name__ == "__main__":
+	main()
+
